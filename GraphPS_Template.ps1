f8a255a94fd43daf33eff66964e1c4eca7d51c6b
himiyo3in <m.chan3in@outlook.com>
2020-12-18 01:13:01 +0900

    add file GraphPS_Template.ps1

---
+++ GraphPS_Template.ps1
@@ -0,0 +1,49 @@
+# 参考情報 (GET)
+# https://www.c-sharpcorner.com/article/how-to-access-microsoft-teams-graph-api-in-powershell/
+
+# 参考情報 (POST)
+# https://www.sharepointeurope.com/getting-started-graph-api-powershell/
+
+############################### ここから テンプレ (Do Not Edit) ###############################
+# Input Parameters  
+$clientId = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"        #アプリケーションID
+$clientSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"      #クライアント シークレット
+$tenantName = "XXXXXXXXXX.onmicrosoft.com"                  #テナント名
+
+$tokenBody = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $clientId
+    Client_Secret = $clientSecret
+}
+$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token" -Method POST -Body $tokenBody
+############################### ここまで テンプレ (Do Not Edit) ###############################
+
+############################### ここから GET メソッド ###############################
+# Get Method
+$URL = "https://graph.microsoft.com/v1.0/users"
+
+$result = Invoke-RestMethod -Headers @{Authorization = "Bearer $($tokenResponse.access_token)"} -Uri $URL -Method Get
+$result
+############################### ここまで GET メソッド ###############################
+
+############################### ここから POST メソッド ###############################
+# # POST Method
+# $URL = "https://graph.microsoft.com/v1.0/groups"
+
+# $postBody = @'
+# {
+#     "description": "Self help community for library",
+#     "displayName": "Library Assist",
+#     "groupTypes": [
+#       "Unified"
+#     ],
+#     "mailEnabled": true,
+#     "mailNickname": "library",
+#     "securityEnabled": false
+# }
+# '@
+
+# $result = Invoke-RestMethod -Headers @{Authorization = "Bearer $($tokenResponse.access_token)"} -Uri $URL -Method POST -Body $postBody -ContentType 'application/json'
+# $result
+############################### ここまで POST メソッド ###############################
